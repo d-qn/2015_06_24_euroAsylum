2d8413f2669fb3ca92d8d2804490861f4417ed5c
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2015-06-29 21:37:54 +0200

    first chart finally!

--- 01_euAsylumRequest.R
+++ 01_euAsylumRequest.R
@@ -40,6 +40,10 @@ data.m2 %>% filter(iso2 == 'CH', citizen == "Total")
 
 data.mm <- rbind(data.m1, data.m2)
 
+# Display which countries were not in the first timeseries
+unique(data.m2$geo)[!unique(data.m2$geo) %in% unique(data.m1$geo)]
+#Croatia                       Liechtenstein
+
 ############################################################################################
 ###		1 AREA chart by host country
 ############################################################################################
@@ -47,8 +51,9 @@ data.mm <- rbind(data.m1, data.m2)
 ## Get for each country the total value, exclude regions iso2 and uncomplete year
 data <- data.mm %>% filter(citizen == "Total", ! iso2 %in% c('EU27', 'EU28', 'TOTAL'), time >= 1986)
 
-ggplot(data = data %>% group_by(time) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()) +
-	geom_line(aes(time, sum))
+
+tot <- data %>% group_by(time) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()
+ggplot(data = tot) + geom_line(aes(time, sum))
 
 	### EXPLORE
 # ggplot(data = data %>% group_by(time, iso2) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()) +
@@ -62,7 +67,6 @@ iso2.top <- unlist(head(as.data.frame(dev[order(dev$sum, decreasing = T),'iso2']
 dd <- data %>% select(one_of(c('geo', 'time', 'values', 'iso2')))
 dd$iso2 <- ifelse(as.character(dd$iso2) %in% iso2.top, as.character(dd$iso2), 'autres')
 
-dd.1 <- dd %>% group_by(time, iso2) %>% mutate (y = sum(values, na.rm = T)) %>% ungroup()
 dd <- dd %>% group_by(time, iso2) %>% summarise (y = sum(values, na.rm = T)) %>% ungroup()
 
 
@@ -82,58 +86,81 @@ collapsedCountries <- paste(as.character(unique(data$geo)[!unique(data$geo) %in%
 ### CHART AREA
 a <- Highcharts$new()
 a$chart(zoomType = "xy", type = 'area', height = 500, spacing = 5)
-hSeries <- hSeries2(data.frame(x = dd$time, y = dd$y, name = dd$iso2, series = dd$geo), "series")
+hSeries <- hSeries2(data.frame(x = dd$time, y = dd$y, name = dd$geo, series = dd$iso2), "series")
+
 a$series(hSeries)
 a$colors(swi_pal)
-
-
-
-
-
-
-
-
-
-
-h <- hPlot(y ~ time, data = dd ,type = c('area'), group = 'iso2', visible = 'visible')
-h$colors(swi_pal)
-h$plotOptions(area = list(stacking = "normal", marker = list(enabled = FALSE, symbol = "circle", radius = 0.1)))
-h$legend(borderWidth= 0)
-h$xAxis(title = list(text = ""), max = max(dd$time), min = min(dd$time),
+a$plotOptions(area = list(stacking = "normal", lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 0.5)))
+a$legend(borderWidth= 0)
+a$xAxis(title = list(text = ""), max = max(dd$time), min = min(dd$time),
 	plotLines = list(list(color = "grey", value = 1992, width = 1, zIndex = -10,
 	label = list(text = "Conflit Ex-Yougoslavie", rotation = 0,
 	style = list( color = 'lightgrey')))))
-h$lang( numericSymbols= NULL)
-h$yAxis(title = list(text = "Demandeurs d'asile en milliers"),
+a$lang( numericSymbols= NULL)
+a$yAxis(title = list(text = "Demandeurs d'asile en milliers"),
 	labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
-h
 
-# plotLines !!!!!!
+	## TO DO tooltip!
+
+
+
+save(dd, collapsedCountries, file = "prod/01_areaChart.Rdata")
 
 ############################################################################################
 ###		2 AREA chart by origin country
 ############################################################################################
-data.m2 <- datl %>% filter(asyl_app == 'Asylum applicant', sex == 'Total', age == 'Total') %>%
-	select(one_of(c('citizen', 'geo', 'time', 'values', 'iso2')))
 
-	### CHECK ###
-## check CH value with http://ec.europa.eu/eurostat/tgm/refreshTableAction.do?tab=table&plugin=1&pcode=tps00191&language=en
-data.m2 %>% filter(iso2 == 'TOTAL', citizen == "Syria")
+# There is only precise country of origin across EU since 2008
 
-## Get for each country the total value   !!!!!!!!!!
-data <- data.m1 %>% filter(citizen == "Total", ! iso2 %in% c('EU28', 'TOTAL'))
-ggplot(data = data %>% group_by(time) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()) +
-	geom_line(aes(time, sum))
+data.m2 %>% filter(iso2 == "TOTAL", time == 2014) %>% top_n(20, values)
+
+data <- data.m2 %>% filter(iso2 == "TOTAL", ! citizen %in% c('Extra EU-28', 'European Union (28 countries)', 'Total'))
+
+tot2 <- data %>% group_by(time) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()
+# check: compare with as.data.frame(tot) !!
+
+# find the n largest countries during the last year
+dev <- data %>% filter(time == max(time)) %>% group_by(citizen) %>% summarise(sum = sum(values, na.rm = T)) %>% ungroup()
+cit.top <- unlist(head(as.data.frame(dev[order(dev$sum, decreasing = T),'citizen']), 10))
+
+
+### Merge not top countries
+ddd <- data %>% select(one_of(c('citizen', 'time', 'values')))
+ddd$citizen <- ifelse(as.character(ddd$citizen) %in% cit.top, as.character(ddd$citizen), 'autres')
+
+ddd <- ddd %>% group_by(time, citizen) %>% summarise (y = sum(values, na.rm = T)) %>% ungroup()
 
 
 
+b <- Highcharts$new()
+b$chart(zoomType = "xy", type = 'area', height = 500, spacing = 5)
+hSeries <- hSeries2(data.frame(x = ddd$time, y = ddd$y, name = ddd$citizen, series = ddd$citizen), "series")
 
-
+b$series(hSeries)
+b$colors(swi_pal)
+b$plotOptions(area = list(stacking = "normal", lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 0.5)))
+b$legend(borderWidth= 0)
+b$xAxis(title = list(text = ""), max = max(ddd$time), min = min(ddd$time))
+b$lang( numericSymbols= NULL)
+b$yAxis(title = list(text = "Demandeurs d'asile en milliers"),
+	labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
 
 
 ############################################################################################
-###		Prior 2008
+###		3 WAFFLE chart by origin country of others
 ############################################################################################
+library(waffle)
+
+ee <- data %>% filter(time == 2014) %>% select(one_of(c('citizen', 'values')))
+
+ee$pc <- ee$values / sum(ee$values) * 100
+
+ww <- ee[which(ee$pc >= 1.5),]
+ww$n <- round(ww$pc)
+wf <- structure(ww$n, names = as.character(ww$citizen))
+wf<- c(wf, `autres` = 100-sum(wf))
+
+waffle(wf, rows = 20)
 
 
 
---
+++ prod/01_areaChartEU.R
@@ -0,0 +1,112 @@
+library(swiRcharts)
+library(swiTheme)
+
+############################################################################################
+###		Load data
+############################################################################################
+
+load("01_areaChart.Rdata")
+trad <- read.csv("01_areaChartEU.csv", check.names = F, stringsAsFactors = F, row.names = 1)
+
+## geo country names check ##
+origin <- unique(dd$geo)
+idxrow.fullgeo <- grep("^full", rownames(trad))
+stopifnot(all(origin %in% gsub("^full\\.", "", rownames(trad)[idxrow.fullgeo])))
+
+############################################################################################
+###		Settings chart
+############################################################################################
+
+chartHeight <- 500
+
+# PlotLines
+plotLinesColor <- "#e5dbcd"
+styleLine <- 'LongDash'
+zIndex <- -10
+widthLine <- 0.5
+yoffset <- 15
+fontSize <-  '0.7em'
+textColor <- "#aa8959"
+
+############################################################################################
+###		chart data
+############################################################################################
+
+i <- 1
+
+
+for (i in 1:ncol(trad)) {
+
+	lang <- colnames(trad)[i]
+	output.html <- paste("01_EUayslumRequests_area_", lang, ".html", sep ="")
+
+	ddd <- dd
+
+	## Translate country names ##
+	ddd[which(ddd$iso2 == "autres"), 'iso2'] <- as.character(trad['iso.others', lang])
+	# create a named vector: new geo, name old geo
+	geotrad <- structure(as.character(trad[idxrow.fullgeo,lang]), names =  gsub("^full\\.", "", rownames(trad)[idxrow.fullgeo]))
+	ddd$geo <- geotrad[match(ddd$geo, names(geotrad))]
+
+
+	## create fancy tooltip as html table
+	ddd$name <- paste0(
+		'<table cellpadding="1" style="line-height:1.4">',
+	        '<tr><td><div style="font-size:0.85em"><b>', ddd$time, '</b></td></div>',
+				'<td></td><td></td></tr>',
+	       '<tr><td colspan="3"><div style="font-size:0.75em">', ddd$y, " ",
+		   		trad["tooltip.ayslumdemand",lang], '</div></td></tr>',
+	       '<tr><td align="left"><div style="font-size:0.75em"><i>', ddd$geo,'</i></td><td></td>',
+		   		'<td style="text-align:right"><div style="color:#D8D8D8;font-size:0.7em">', ddd$iso2,'</div></td></tr>',
+		'</table>')
+
+
+	## CHART
+	a <- Highcharts$new()
+	a$chart(zoomType = "xy", type = 'area', height = chartHeight, spacing = 5)
+	hSeries <- hSeries2(data.frame(x = ddd$time, y = ddd$y, name = ddd$name, series = ddd$iso2), "series")
+
+	a$series(hSeries)
+	a$colors(swi_pal)
+	a$plotOptions(area = list(stacking = "normal", lineWidth = 0, marker = list(enabled = FALSE, symbol = "circle", radius = 0.5)),
+		series = list(fillOpacity = 1))
+
+	a$legend(borderWidth= 0, itemMarginTop = 3, itemMarginBottom = 5, itemHoverStyle = list(color = '#996666'),
+		itemStyle = list(fontWeight = "normal", fontSize = "0.8em"),
+		title = list(style = list(fontWeight ='normal'),
+			text = paste0(trad['legend.country',lang], ' <span style="font-size: 9px; color: #666; font-weight: normal">',
+			trad['legend.descr',lang], '</span><br>')), style = list(fontStyle = 'italic'))
+
+	a$xAxis(title = list(text = ""), max = max(dd$time), min = min(dd$time),
+		plotLines = list(
+			list(color = plotLinesColor, value = 1991.4, width = widthLine, zIndex = zIndex, dashStyle = styleLine,
+			label = list(text = trad['annotation.exYougoslavia',lang], rotation = 0, y = yoffset,
+			style = list( color = '#D8D8D8', fontSize = fontSize))),
+			list(color = plotLinesColor, value = 1998.2, width = widthLine, zIndex = zIndex, dashStyle = styleLine,
+			label = list(text = trad['annotation.Kosovo',lang], rotation = 0, y = yoffset,
+			style = list( color = '#D8D8D8', fontSize = fontSize))),
+			list(color = plotLinesColor, value = 2011.3, width = widthLine, zIndex = zIndex, dashStyle = styleLine,
+			label = list(text = trad['annotation.Syria',lang], rotation = 0, x = -5, y = yoffset, align = 'right',
+			style = list( color = '#D8D8D8', fontSize = fontSize)))
+			)
+		)
+
+	a$lang( numericSymbols= NULL)
+	a$yAxis(title = list(text = trad['y.lab',lang]), gridLineColor = "#EFEFEF",
+		labels = list(formatter = "#! function () {return this.value / 1000;} !#"))
+
+	# formatter <- "#! function() { return '<b>' +
+	# 	this.point.x + '</b><br><br>' + this.point.y + '<br>' + this.point.name + ' ('+ this.series.name + ')</div>'; } !#"
+	# a$tooltip(formatter = formatter, useHTML = T, borderWidth = 2, backgroundColor = 'rgba(255,255,255,0.8)')
+	a$tooltip(formatter = "#! function() { return this.point.name; } !#", useHTML = T , borderWidth = 3, style = list(padding = 1.5))
+	a
+
+	hChart.html <- tempfile("hChart_area")
+	a$save(hChart.html)
+
+	# Convert highcharts-rCharts html chart into a responsive one
+
+	hChart2responsiveHTML(hChart.html, output.html = output.html, h2 = trad['title',lang], descr = trad['descr',lang],
+		source = trad['source',lang], h3 = "", author = "Duc-Quang Nguyen | swissinfo.ch")
+
+}
