bf6a3f2b60dc1cbf48ea19bbdaf756e388fd671a
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2015-07-03 22:07:57 +0200

    tmp pre-weekend commit

--- 03_citizenMonthly_wafflePlot.R
+++ 03_citizenMonthly_wafflePlot.R
@@ -11,7 +11,8 @@ library(png)
 inputData <- "data/02_citizenMonthly_waffled.Rdata"
 load(inputData)
 
-tradFile <- "prod/waffle"
+tradFile <- "prod/02_translation.csv"
+trad <- read.csv(tradFile, check.names = F, stringsAsFactors = F, row.names = 1)
 
 # load SWI logo
 swiLogo <- readPNG("~/swissinfo/_helpers/SWI-RGB.png")
@@ -75,22 +76,39 @@ waffled <- function (parts, rows = 10, xlab = NULL, title = NULL, colors = NA,
     gg <- gg + theme(plot.title = element_text(size = 18))
     gg <- gg + theme(plot.background = element_blank())
 	## Change 4 make space on the top and comment the other line
-    gg <- gg + theme(plot.margin = unit(c(4, 0, 0, 0), "lines"))
+    gg <- gg + theme(plot.margin = unit(c(2.8, 0, 0.5, 0), "lines"))
     # gg <- gg + theme(plot.margin = rep(unit(0, "null"), 4))
     gg
 }
 
 
 ############################################################################################
-###
+### subset the whole dataset and save the production data.frame
 ############################################################################################
 
 range(data$time)
 tmp <- as.data.frame(df %>% filter(iso2 != 'EU28') %>% group_by(iso2) %>%
-	summarise(totsq = sum(sq)) %>% filter(totsq > 2 | iso2 %in% c('PT', 'ES') |
-	!iso2 %in% c('LI', 'IS', 'CZ', 'HR', 'LT', 'LV', 'SI', 'SK', 'EE')))
+	summarise(totsq = sum(sq)) %>% filter(totsq > 2 | iso2 %in% c('PT', 'ES')) %>%
+	filter(!iso2 %in% c('LI', 'IS', 'CZ', 'HR', 'LT', 'LV', 'SI', 'SK', 'EE', 'BG', 'LU', 'PT', 'BE', 'FI', 'IE')))
 
 iso.ordered <- as.character(tmp$iso2[order(tmp$totsq)])
+
+## Create the translation data.frame
+t.df <- df %>% filter(iso2 %in% iso.ordered) %>% select(iso2, geo, citizen)
+
+t.df <- rbind(unique(t.df[,1:2]), data.frame(iso2 = paste0("cit.", substr(unique(t.df$citizen), 1, 4)), geo = unique(t.df$citizen)))
+colnames(t.df) <- c('code', 'en')
+write.csv(t.df, file = "prod/02_translation_tmp.csv", row.names = F)
+
+
+### Add the citizen code to data.frame
+df <- cbind(df, cit.code = t.df[match(df$citizen, t.df$en), 'code'])
+rownames(df) <- NULL
+
+write.csv(df, file = "prod/02_inputData.csv")
+
+
+
 
 ############################################################################################
 ###		Plot
@@ -100,31 +118,40 @@ iso.ordered <- as.character(tmp$iso2[order(tmp$totsq)])
 font <- "Open Sans"
 # how many rows in waffle
 w.row <- 10
+w.size <- 3.7
+
+legendKeySize <- unit(2, "line")
+legendKeyHeight <- unit(1.6,"line")
+animationInterval <- 4.2
 
 
 colorV <- structure(swi_rpal[1:nlevels(df$citizen)], names = levels(df$citizen))
 
-iso <- 'HU'
+iso <- 'HU' ## debugging
+
 waffleIso <- function(iso = 'CH') {
 	stopifnot(length(iso) == 1)
 
 	dff <- df %>% filter(iso2 == iso)
+	dfff <- dff %>% filter(sq > 0)
+	wf <- structure(dfff$sq, names = as.character(dfff$citizen))
 
-	countryTop <- geom_text(data = data.frame(x = 0, y = w.row + 2, label = dff$geo[1]), aes(x = x, y = y, label = label),
-				family = font, fontface = "bold", alpha = 0.4, size = 11, hjust = 0, vjust = 0, colour = "#aa8959")
-	topText <- paste0(dff[which(dff$citizen == "Total"),'sum'],
+	countryTop <- geom_text(data = data.frame(x = 0, y = w.row + 2.1, label = dff$geo[1]), aes(x = x, y = y, label = label),
+				family = font, fontface = "bold", alpha = 1, size = 11, hjust = 0, vjust = 0, colour = "#aa8959")
+	topText <- paste0(dff[which(dff$citizen == "Total"),'sum'], " ",
 		" demandes d'asile y ont été déposées en 2015 jusqu'à mars.")
-	titleTop <- geom_text(data = data.frame(x = 0, y = w.row + 1.4, label = topText), aes(x = x, y = y, label = label),
+	titleTop <- geom_text(data = data.frame(x = 0, y = w.row + 1.45, label = topText), aes(x = x, y = y, label = label),
 				family = font, alpha = 1, size = 5, hjust = 0, vjust = 0)
-	text2 <- paste0("Si la ",  dff$geo[1], " avait ", unit,  " habitants, il y aurait eu ",
-		dff[which(dff$citizen == "Total"),'perU'], " demandes d'asile provenant de:")
+	text2 <- paste0("Si ce pays avait", " ", unit,  " ", "habitants, il y aurait eu", " ",
+		dff[which(dff$citizen == "Total"),'perU'], " ", "demandes d'asile provenant de:")
 	subtitle <- geom_text(data = data.frame(x = 0, y = w.row + 0.85, label = text2), aes(x = x, y = y, label = label),
-				family = font, fontface = "bold", alpha = 1, size = 5, hjust = 0, vjust = 0)
+				family = font, alpha = 1, size = 5, hjust = 0, vjust = 0)
 
 	# Hack: set a blank waffle and display top text
-	blankW <- waffled(rep(round(17 * w.row / length(wf)), length(wf)), rows = w.row, size = 0.7, colors = rep("white", length(wf))) +
-		theme(legend.position = "bottom", legend.key.size = unit(0.7, "line"), legend.key.height = unit(1,"line"),
-		legend.key = element_rect(colour = NA), legend.text = element_text(colour = "white")) + guides(fill = guide_legend(nrow = 2, byrow = TRUE,
+	blankW <- waffled(rep(round(17 * w.row / length(wf)), length(wf)), rows = w.row, size = w.size, colors = rep("white", length(wf))) +
+		theme(legend.position = "bottom", legend.key.size = legendKeySize, legend.key.height = legendKeyHeight,
+		legend.key = element_rect(colour = NA), legend.text = element_text(colour = "white")) +
+		guides(fill = guide_legend(nrow = 2, byrow = TRUE,
 		override.aes = list(colour = NULL)))
 	textOnly <- blankW + countryTop +titleTop + subtitle
 
@@ -134,13 +161,12 @@ waffleIso <- function(iso = 'CH') {
 	grid.draw(textOnly2)
 
 	# Waffle chart
-	wf <- structure(dff$sq, names = as.character(dff$citizen))
 	padding <- maxCol - ceiling(sum(wf) / w.row)
-	gw <- waffled(wf, rows = w.row, size = 0.7, colors = unname(colorV[match(names(wf), names(colorV))]),
+	gw <- waffled(wf, rows = w.row, size = w.size, colors = unname(colorV[match(names(wf), names(colorV))]),
 		pad = padding, xlab = NULL) +
-		theme(legend.position = "bottom", legend.key.size = unit(0.7, "line"), legend.key.height = unit(1,"line"),
+		theme(legend.position = "bottom", legend.key.size = legendKeySize, legend.key.height = legendKeyHeight,
 		legend.key = element_rect(colour = NA),
-		axis.ticks = element_blank(), axis.text = element_blank(), text = element_text(size = 13, family = font)) +
+		axis.ticks = element_blank(), axis.text = element_blank(), text = element_text(size = 14, family = font)) +
 		guides(fill = guide_legend(nrow = 2, byrow = TRUE, override.aes = list(colour = NULL)))
 		# TITLE
 
@@ -151,7 +177,19 @@ waffleIso <- function(iso = 'CH') {
 	gw3$layout$clip[gw3$layout$name == "panel"] <- "off"
 	grid.newpage()
 	grid.draw(gw3)
+	grid.newpage()
 	grid.draw(gw3)
+}
+
+
+
+introText <- function(title, title2, subtitle, img = swiLogo) {
+	par(mar = c(0,0,0,0))
+	plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+	text(x = 0.01, y = 0.95, title, cex = 2.3, col = "black", family = font, font = 2, adj = 0)
+	text(x = 0.01, y = 0.88, title2, cex = 2.3, col = "black", family = font, font = 2, adj = 0)
+	text(x = 0.01, y = 0.5, subtitle, cex = 2.3, col = "gray30", family = font, font = 1, adj = 0)
+	rasterImage(img, .85, 0.03, 0.96, 0)
 }
 
 test <- T
@@ -161,18 +199,10 @@ if(test){
 	output <- "02_EU_asylum_waffle.gif"
 }
 
-introText <- function(title, subtitle, img = swiLogo) {
-	par(mar = c(0,0,0,0))
-	plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-	text(x = 0.05, y = 0.95, title, cex = 2.2, col = "black", family = font, font = 2, adj = 0)
-	text(x = 0.05, y = 0.88, subtitle, cex = 2, col = "gray30", family = font, font = 1, adj = 0)
-	rasterImage(img, .85, 0.05, 0.96, 0)
-}
-
 
 
 saveGIF({
-	introText(title = "Demandes d'asile en Europe", "Si tous les pays européens avait 50000 habitants...")
+	introText(title = "Demandes d'asile en Europe", "En 2015, jusqu'à présent", "Si tous les pays européens avait 50000 habitants...")
 	if(test) {
 		wp <- waffleIso('CH')
 		print(wp)
@@ -186,7 +216,7 @@ saveGIF({
 			print(wp)
 		}
 	}
-}, movie.name = output, interval = 5, nmax = 50, ani.width = 800, ani.height = 600, loop = TRUE, outdir = "prod")
+}, movie.name = output, interval = animationInterval, nmax = 50, ani.width = 800, ani.height = 600, loop = TRUE, outdir = "prod")
 
 
 
